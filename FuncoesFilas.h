cff41f1e4381ce1d687345f2fe06e0b382702189
Julia <juliavsb@dcc.ufrj.com>
2024-11-13 20:14:57 -0300

    removi as funçoes do codigo antigo

--- FuncoesFilas.h
+++ FuncoesFilas.h
@@ -1,9 +1,9 @@
 #include <stdio.h>
-#include <time.h>
+#include <stdbool.h>
 #include <stdlib.h>
 
 #define MAX_PROCESSOS 5
-#define QUANTUM 4
+
 
 typedef struct Processo{
     int pid;
@@ -24,8 +24,14 @@ typedef struct Fila {
     No_Fila *tras;
 } Fila;
 
+/*le o proximo inteiro da linha separada por ";"*/
+
+int prox_inteiro(char *linha, int *inicio){
+
+}
+
 /*função manipulação fila*/
-Fila* inicializa_fila(Fila* f){
+Fila* inicializa_fila(){
     Fila *f = (Fila *) malloc(sizeof(Fila));
     f->frente = NULL;
     f->tras = NULL;
@@ -38,7 +44,7 @@ int fila_vazia(Fila* f){
     return 0;
 }
 
-void enfileirar_processos(Fila* f, Processo* p, int time){
+void enfileirar_processos(Fila* f, Processo* p, int tempo){
     if(fila_vazia(f)){
         f -> frente = (No_Fila*) malloc(sizeof(No_Fila));
         f -> frente -> p = p; 
@@ -79,6 +85,9 @@ void desinfileirar_processos(Fila* f){
 }
 
 
+
+
+
 Processo* cria_processo(int pid){
     Processo* p;
     int tipo_io;
@@ -108,96 +117,10 @@ Processo* cria_processo(int pid){
     return p;
 }
 
-/*CÓDIGO ANTIGO N ALTERADO*/
 
-void executa_processo(Processo* p, Fila* fila_destino){
-    if(p->tempo_restante > QUANTUM){
-        p->tempo_restante -= QUANTUM;
-        enfileirar_processos(fila_destino, p);
-    }
-    else{
-        p->tempo_restante = 0;
-        printf("Processo %d finalizado", p->pid);
-        free(p);
-    }
-}
 
- 
-void gerencia_escalonamento(Fila* alta_prioridade, Fila* baixa_prioridade, Fila fila_io[]){
-    int i;
 
-    while(!fila_vazia(alta_prioridade) || !fila_vazia(baixa_prioridade) || !fila_vazia(&fila_io[0]) || !fila_vazia(&fila_io[1]) || !fila_vazia(&fila_io[2])){
-        Processo* processo;
 
-        /*Executa processos na fila de alta prioridade*/
-        if(!fila_vazia(alta_prioridade)){
-            processo = desinfileirar_processos(alta_prioridade);
-            executa_processo(processo,baixa_prioridade);
-        }
 
-        /*Executa processos na fila de baixa prioridade*/
-        else if(!fila_vazia(baixa_prioridade)){
-            processo = desinfileirar_processos(baixa_prioridade);
-            executa_processo(processo, baixa_prioridade);
-        }
 
-        /*Processa filas de I/O*/
-        for(i = 0; i < 3; i++){
-            if(!fila_vazia(&fila_io[i])){
-                processo = desinfileirar_processos(&fila_io[i]);
 
-                if(processo->tipo_io == 'd'){
-                    enfileirar_processos(baixa_prioridade, processo);
-                }
-                else{
-                    enfileirar_processos(alta_prioridade, processo);
-                }
-            }
-        }
-        printf("\n");
-        printf("\n");
-        printf("\n");
-        printf("processo %d", processo->pid);
-        printf("\n");
-        printf("tempo_restante: %d", processo->tempo_restante);
-        printf("\n");
-    }
-}
-
-int main(){
-
-    
-    Fila alta_prioridade, baixa_prioridade;
-    Fila fila_io[3] ; /*0 = disco, 1 = fita magnetica, 2 = impressora*/
-    int i, j;
-    srand(time(NULL));
-    
-
-    inicializa_fila(&alta_prioridade);
-    inicializa_fila(&baixa_prioridade);
-
-    for(i = 0; i < 3; i++){
-        inicializa_fila(&fila_io[i]);
-    }
-
-    /*cria o processo e adiciona na fila de alta prioridade*/
-    for(j = 1; j <=MAX_PROCESSOS; j++){
-        Processo* novo_processo;
-        novo_processo = cria_processo(j);
-        printf("%d processo criado!\n\n", novo_processo->pid);
-        printf("Processo %d\n", novo_processo->pid);
-        printf("tempo servico: %d\n", novo_processo->tempo_servico);
-        printf("tempo restante: %d\n", novo_processo->tempo_restante);
-        printf("tipo io: %c\n", novo_processo->tipo_io);
-        printf("tempo espera io: %d\n\n", novo_processo->tempo_espera_io);
-        enfileirar_processos(&alta_prioridade, novo_processo);
-
-    }
-    
-    /*Executa o escalonador*/
-    gerencia_escalonamento(&alta_prioridade, &baixa_prioridade, fila_io);
-
-    return 0;
-} 
-
-/*arrumar os printfs pra entender de fato qq esta acontecendo no algoritmo*/
